58572eb15649aeb57963d81f92b26a0a80a2e325
Unknown <massimoboettesta@enti.cat>
2018-06-16 00:21:16 +0200

    Added Materials + Menu Scene Started

---
+++ FallingCED/Assets/Scripts/MenuTextEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuTextEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		GetComponent<Text> ().text = ""+Random.Range (1, 10);
+	}
+}
